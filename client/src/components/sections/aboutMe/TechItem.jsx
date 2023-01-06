4e322e88f0d27bfc8c16c563691bd1532970959b
Kuba <kuba@MacBook-Air-Kuba.local>
2023-01-06 14:09:14 +0100

    created about me section

--- client/src/components/sections/aboutMe/TechItem.jsx
+++ client/src/components/sections/aboutMe/TechItem.jsx
@@ -2,9 +2,15 @@ import React from "react";
 
 function TechItem({ icon, name }) {
     return (
-        <div className="w-32 h-32 bg-[#16191c] flex flex-col justify-around items-center rounded-lg">
-            <img src={icon} alt="Technology icon" className="h-16 w-16" />
-            <p className="font-IBM">{name}</p>
+        <div className="w-32 h-32 bg-[#16191c] flex flex-col justify-around items-center rounded-lg group transition-all duration-150 ease-in-out">
+            <img
+                src={icon}
+                alt="Technology icon"
+                className="h-16 w-16 z-0 grayscale group-hover:grayscale-0 duration-150 ease-in-out"
+            />
+            <p className="font-IBM invisible group-hover:visible text-accent duration-150 ease-in-out">
+                {name}
+            </p>
         </div>
     );
 }
