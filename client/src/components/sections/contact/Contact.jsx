c821583f82513b3949375d62ae905b069403a53a
j-lewandowski <kubalew.pl@gmail.com>
2023-01-08 23:22:20 +0100

    addded contact section

--- client/src/components/sections/contact/Contact.jsx
+++ client/src/components/sections/contact/Contact.jsx
@@ -1,9 +1,11 @@
-import React from "react";
-
 function Contact() {
     return (
-        <div className="h-screen w-full grid grid-cols-1 md:grid-cols-2 font-oxygen text-white">
-            Contact
+        <div className="h-screen w-full flex flex-col justify-center items-center font-oxygen text-white relative space-y-16">
+            <p className="font-IBM text-3xl font-bold">Napisz do mnie</p>
+            <button className="p-4 bg-[#16191c] rounded-md font-IBM hover:text-accent">
+                Napisz wiadomość
+            </button>
+            <div className="absolute"></div>
         </div>
     );
 }
