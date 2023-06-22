5c2d302b46e6d9c8343650b9dd0e5f58f487ec8c
j-lewandowski <kubalew.pl@gmail.com>
2023-06-23 01:14:42 +0200

    hidden calculator

--- client/src/pages/MainPage.jsx
+++ client/src/pages/MainPage.jsx
@@ -5,15 +5,15 @@ import Calculator from "../components/calculator/Calculator";
 import Contact from "../components/sections/contact/Contact";
 
 function MainPage({ projects }) {
-    return (
-        <div className="w-screen flex flex-col items-center justify-center">
-            <Main />
-            <AboutMe />
-            <MyProjects id="my-projects" projects={projects} />
-            <Calculator />
-            <Contact id="contact" />
-        </div>
-    );
+  return (
+    <div className="w-screen flex flex-col items-center justify-center">
+      <Main />
+      <AboutMe />
+      <MyProjects id="my-projects" projects={projects} />
+      {/* <Calculator /> */}
+      <Contact id="contact" />
+    </div>
+  );
 }
 
 export default MainPage;
